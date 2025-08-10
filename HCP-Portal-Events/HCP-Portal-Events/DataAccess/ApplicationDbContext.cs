ec07e1c38242df5bd82c4c709499ac91c6d30844
mourad2213 <ahmed.mourad338@gmail.com>
2025-08-10 14:33:34 +0300

    add strucure to main

---
+++ HCP-Portal-Events/HCP-Portal-Events/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MyApiProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
