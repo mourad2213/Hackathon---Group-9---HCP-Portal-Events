4e5ad35df2f2a8b1dc4a142a00821cf92444a706
mourad2213 <ahmed.mourad338@gmail.com>
2025-08-10 14:56:03 +0300

    Add Activity/ActivityType Models

---
+++ HCP-Portal-Events/HCP-Portal-Events/Models/Activity.cs
@@ -0,0 +1,23 @@
+﻿namespace HCP_Portal_Events.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public String Speaker {  get; set; }
+
+        public int no {  get; set; }
+
+        public int ActivityId { get; set; }
+        public ActivityType ActivityType { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+    }
+}
---
+++ HCP-Portal-Events/HCP-Portal-Events/Models/ActivityType.cs
@@ -0,0 +1,8 @@
+﻿namespace HCP_Portal_Events.Models
+{
+    public class ActivityType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
