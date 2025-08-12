f7347b6021e30d1caffe95f1ac38b0d710f03afa
John-Nakhla <waeljohn306@gmail.com>
2025-08-12 10:33:15 +0300

    WIP: save local changes before merge

---
+++ HCP-Portal-Events/HCP-Portal-Events/DataAccess/Interfaces/IUserRepositiories.cs
@@ -0,0 +1,17 @@
+﻿using HCP_Portal_Events.Models;
+using HCP_Portal_Events.Models.DTOs;
+using System.Collections.Generic;
+using static HCP_Portal_Events.Models.DTOs.UserDTO;
+
+namespace HCP_Portal_Events.DataAccess.Interfaces
+{
+    public interface IUserRepositiories
+    {
+        Task<User> GetUserByIdAsync(int id);
+        Task<bool> UpdateUserAsync(int id, UserUpdateDto userUpdateDto);
+
+        Task<ICollection<Event>> GetUserPerviousEvents(int userId);
+        Task<ICollection<Event>> GetUserUpcomingEvents(int userId);
+        Task<ICollection<Event>> GetUserSpecialityEvents(User user);
+    }
+}
