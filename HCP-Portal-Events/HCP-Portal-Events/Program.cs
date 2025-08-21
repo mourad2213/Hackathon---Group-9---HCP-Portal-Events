733ccdbd04f7cde8509efc1cb2d9512d3265300f
John-Nakhla <waeljohn306@gmail.com>
2025-08-21 13:52:59 +0300

    allow CORS and edit seeding data

--- HCP-Portal-Events/HCP-Portal-Events/Program.cs
+++ HCP-Portal-Events/HCP-Portal-Events/Program.cs
@@ -2,15 +2,12 @@ using HCP_Portal_Events.DataAccess.Interfaces;
 using HCP_Portal_Events.DataAccess.Reposatiores;
 using HCP_Portal_Events.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 using MyApiProject.Data;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -18,7 +15,20 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
         options.JsonSerializerOptions.WriteIndented = true;
     });
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>((provider, options) =>
 {
     options.UseSqlServer(
@@ -29,9 +39,9 @@ builder.Services.AddDbContext<ApplicationDbContext>((provider, options) =>
                 maxRetryCount: 3,
                 maxRetryDelay: TimeSpan.FromSeconds(10),
                 errorNumbersToAdd: null);
-
         });
 });
+
 builder.Services.AddScoped<IUserRegistrationRepository, UserRegistrationRepository>();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IUserRepositiories, UserRepositories>();
@@ -40,15 +50,23 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
 
+app.UseCors("AllowAll");
+
 app.MapControllers();
+
+// Redirect root ("/") to Swagger
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger/index.html");
+    return Task.CompletedTask;
+});
 
 app.Run();
