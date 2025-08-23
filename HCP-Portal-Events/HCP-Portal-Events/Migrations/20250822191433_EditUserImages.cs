ef6fc11607c20c66c44469910e1e841e3a335088
John-Nakhla <waeljohn306@gmail.com>
2025-08-23 19:50:51 +0300

    edit seeding

---
+++ HCP-Portal-Events/HCP-Portal-Events/Migrations/20250822191433_EditUserImages.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HCP_Portal_Events.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditUserImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ProfilePicture",
+                value: "//images/users/download.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ProfilePicture",
+                value: "/images/users/download.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ProfilePicture",
+                value: "/images/users/download.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ProfilePicture",
+                value: "/images/users/download.webp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ProfilePicture",
+                value: "/images/users/OIP_1.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ProfilePicture",
+                value: "/images/users/OIP_2.webp");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ProfilePicture",
+                value: "/images/users/OIP_3.jpeg");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ProfilePicture",
+                value: "/images/users/OIP_4.webp");
+        }
+    }
+}
